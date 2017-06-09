676983924661089863a20c1d53d8a9e060188e4a
Brenan <barlebino@tutanota.com>
2017-06-08 17:29:44 -0700

    Thug sprites in and collision of thugs changed

--- Exit.gd
+++ Exit.gd
@@ -9,15 +9,23 @@ export var dest = ""
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	set_fixed_process(true)
-	set_enable_monitoring(true)
+	# set_fixed_process(true)
+	# set_enable_monitoring(true)
+	connect("body_enter", self, "on_exit")
 	pass
 
-func _fixed_process(delta):
-	var contactList = get_overlapping_bodies()
-	
-	for i in range(contactList.size()):
-		if(contactList[i].get_name() == "Player"):
-			get_tree().change_scene(dest)
-	
+func on_exit(body):
+	if(body.get_name() == "Player"):
+		get_tree().change_scene(dest)
+
+func _process(delta):
 	pass
+
+# func _fixed_process(delta):
+	# var contactList = get_overlapping_bodies()
+	
+	# for i in range(contactList.size()):
+		# if(contactList[i].get_name() == "Player"):
+			# get_tree().change_scene(dest)
+	
+	# pass
