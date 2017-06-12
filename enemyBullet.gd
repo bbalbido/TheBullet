69847d1458a297fbf97b27c8eb26cbb6bce32191
Karthik Jayakumar <kjayakum@calpoly.edu>
2017-06-11 22:53:18 -0700

    Fixed Pathing bullet spawning

--- enemyBullet.gd
+++ enemyBullet.gd
@@ -10,12 +10,9 @@ func _on_enemy_body_enter(body):
 
 func _ready():
 	# Called every time the node is added to the scene.
-	set_contact_monitor(true)
-	#var self_gtrans = self.get_global_transform()
-	#var player_local = self_gtrans.xform_inv(playervariables.get("playerLocation"))
 	get_node("SamplePlayer").play("laser")
 	var Direction = playervariables.get("playerLocation") - get_global_pos()
 	Direction = Direction.normalized()
-	set_pos(get_pos() + (Direction * 60))
+	set_global_pos(get_parent().get_global_pos() + (Direction * 50))
 	set_linear_velocity(Direction * speed)
 	connect("body_enter", self, "_on_enemy_body_enter")
